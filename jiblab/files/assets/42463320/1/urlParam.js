d5e7c5970c09d8c43bc26d7355b549ba506ab742
noobusdeer <hello@noobusdeer.dev>
2021-02-14 22:31:53 +0300

    update head logic
    upd
    
    try fix
    
    tryfixagain
    
    fix3
    
    fix

--- jiblab/files/assets/42463320/1/urlParam.js
+++ jiblab/files/assets/42463320/1/urlParam.js
@@ -1,11 +1,17 @@
 var UrlParam = pc.createScript('urlParam');
 UrlParam.attributes.add('text', { type: 'entity' });
 
-
 UrlParam.prototype.initialize = function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const product = urlParams.get('text');
-    this.text.element.text = product;
+    if(product != null){
+        this.text.element.text = product;
+    }
+    else
+    {
+        this.text.element.text = "На страже цифрового будущего";
+    }
+    
 };
 

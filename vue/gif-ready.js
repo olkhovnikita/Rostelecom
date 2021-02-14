2b14d5d449992055bad2d90cffd1a7cbfb24f77c
noobusdeer <hello@noobusdeer.dev>
2021-02-15 00:05:27 +0300

    add canvas

--- vue/gif-ready.js
+++ vue/gif-ready.js
@@ -3,19 +3,24 @@ var gifReady = Vue.component('gif-ready', ({
     template: `
     <div class='gif-ready-container'>
         <div class='gif-ready-header'>
-            <p class='select gif-ready-text'>Твоя GIF-открытка готова.<br>
-                Сохрани её и отправь коллеге</p>
+            <p class='select gif-ready-text'>Твоя GIF-открытка готова.<br></p>
             <div class='download-button-group'>
                 <a href='#' class='gif-ready-download-btn select-example-button' download>Скачать</a>    
             </div>
         </div>
         <div>
-            <img :src="img" alt="selected picture" class='personalise-example gif-ready-example'>
+            <canvas id="gif" ></canvas>
         </div>
         <button type='button' class='select-example-button make-another-btn' @click="$emit('page-number', 'main-content')">Создать еще одно GIF-поздравление</button>
     </div>
     `,
+    mounted() {
+        let externalScriptFirst = document.createElement('script')
+        externalScriptFirst.setAttribute('src', 'jiblab/__start__.js')
+        document.head.appendChild(externalScriptFirst)
 
-
-
+        let externalScriptSecond = document.createElement('script')
+        externalScriptSecond.setAttribute('src', 'jiblab/__loading__.js')
+        document.head.appendChild(externalScriptSecond)
+    }
 }))

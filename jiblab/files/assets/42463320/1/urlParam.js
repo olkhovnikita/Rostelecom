e0ce52aa590e2ffd48f0aa1e2fe8251aebed7384
noobusdeer <hello@noobusdeer.dev>
2021-02-14 22:31:53 +0300

    update head logic

--- jiblab/files/assets/42463320/1/urlParam.js
+++ jiblab/files/assets/42463320/1/urlParam.js
@@ -6,6 +6,28 @@ UrlParam.prototype.initialize = function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const product = urlParams.get('text');
-    this.text.element.text = product;
+    if(product != null){
+        this.text.element.text = product;
+    }
+    else
+    {
+        this.text.element.text = "На страже цифрового будущего";
+    }
+    
+    const photo = urlParams.get('photo');
+    if(photo != null) {
+        var image = new Image();
+        image.crossOrigin = "anonymous";
+        image.onload = function () {
+            var texture = new pc.Texture(self.app.graphicsDevice);
+            texture.setSource(image);
+
+            var material = self.entity.model.material;
+            material.emissiveMap = texture;
+            material.opacityMap = texture;
+            material.update();
+        };
+        image.src = 'http://23february-rt.com/uploads/' + photo + ".png";
+    }
 };
 

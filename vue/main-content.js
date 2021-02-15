6b489f06e4f81924ac01daef5c33f83db18d2d1c
noobusdeer <hello@noobusdeer.dev>
2021-02-15 12:59:35 +0300

    add gif to start page

--- vue/main-content.js
+++ vue/main-content.js
@@ -4,11 +4,11 @@ var mainContent = Vue.component('main-content', ({
      <div class="content">
         <p class="select">Выбери понравившийся шаблон</p>
         <div class="examples">
-            <div style="background-image:url(img/img_1.png)" :class="template=='Star' ? 'example active' : 'example'" @click="setSceneId('Star')"></div>
-            <div style="background-image:url(img/img_2.png)" :class="template=='Chirlider' ? 'example active' : 'example'" @click="setSceneId('Chirlider')"></div>
-            <div style="background-image:url(img/img_3.png)" :class="template=='rocket' ? 'example active' : 'example'" @click="setSceneId('rocket')"></div>
-            <div style="background-image:url(img/img_4.png)" :class="template=='city' ? 'example active' : 'example'" @click="setSceneId('city')"></div>
-            <div style="background-image:url(img/img_5.png)" :class="template=='Beach' ? 'example active' : 'example'" @click="setSceneId('Beach')"></div>
+            <div style="background-image:url(gif/star.gif)" :class="template=='Star' ? 'example active' : 'example'" @click="setSceneId('Star')"></div>
+            <div style="background-image:url(gif/chir.gif)" :class="template=='Chirlider' ? 'example active' : 'example'" @click="setSceneId('Chirlider')"></div>
+            <div style="background-image:url(gif/rocket.gif)" :class="template=='rocket' ? 'example active' : 'example'" @click="setSceneId('rocket')"></div>
+            <div style="background-image:url(gif/city.gif)" :class="template=='city' ? 'example active' : 'example'" @click="setSceneId('city')"></div>
+            <div style="background-image:url(gif/beach.gif)" :class="template=='Beach' ? 'example active' : 'example'" @click="setSceneId('Beach')"></div>
         </div>
         <div class='select-button'>
             <button type='button' class='select-example-button' @click="$emit('page-number', 'person')" :disabled ='!template'>Выбрать этот шаблон</button>

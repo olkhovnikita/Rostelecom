4d6317aebc10750f989d7431af9127f1d792809e
noobusdeer <hello@noobusdeer.dev>
2021-02-17 14:33:44 +0300

    test

--- final/gif/rostelecomAnimations.js
+++ final/gif/rostelecomAnimations.js
@@ -88,10 +88,8 @@ var Scene = {
     
             a_context.rotate(rad);
             a_context.drawImage(a_msk, a_width/2*(-1), a_height/2*(-1), a_width, a_height);
-            a_context.save();
             a_context.globalCompositeOperation = 'source-in';
             a_context.drawImage(a_img, a_width/2*(-1), a_height/2*(-1), a_width, a_height);
-            a_context.restore();
             a_context.rotate(-1 * rad)
             a_context.translate((a_x+a_width/2) * (-1), (a_y + a_height / 2) * (-1));
     

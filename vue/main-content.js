2486bb34e294e34bebab9cd2b888a7dea6cba192
noobusdeer <hello@noobusdeer.dev>
2021-02-18 11:54:18 +0300

    change first screen

--- vue/main-content.js
+++ vue/main-content.js
@@ -2,7 +2,7 @@ var mainContent = Vue.component('main-content', ({
     props: ['template'],
     template: `       
      <div class="content">
-        <p class="select">Выбери понравившийся шаблон</p>
+        <p class="select">Выбери понравившийся шаблон</br> и нажми на кнопку продолжить</p>
         <div class="examples">
             <div style="background-image:url(gif/star.gif)" :class="template=='Star' ? 'example active' : 'example'" @click="setSceneId('Star')"></div>
             <div style="background-image:url(gif/chir.gif)" :class="template=='Chirlider' ? 'example active' : 'example'" @click="setSceneId('Chirlider')"></div>
@@ -11,7 +11,7 @@ var mainContent = Vue.component('main-content', ({
             <div style="background-image:url(gif/beach.gif)" :class="template=='Beach' ? 'example active' : 'example'" @click="setSceneId('Beach')"></div>
         </div>
         <div class='select-button'>
-            <button type='button' class='select-example-button' @click="$emit('page-number', 'person')" :disabled ='!template'>Выбрать этот шаблон</button>
+            <button type='button' class='select-example-button' @click="$emit('page-number', 'person')" :disabled ='!template'>продолжить</button>
         </div>
     </div>`,
     methods: {
